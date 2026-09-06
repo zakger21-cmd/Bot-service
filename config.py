c9888532d3b20530ec0c8b9b0a5cdec7796693b0
zakapoune <zakger21@gmail.com>
2026-09-05 22:59:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@
 
 # ID de ton serveur Discord (pour synchroniser les commandes instantanément)
 # Clic droit sur le nom du serveur (icône en haut à gauche) > Copier l'identifiant
-GUILD_ID = 123456789012345678
+GUILD_ID = 1545840346716577953
 
 # ID du rôle "Staff" qui peut gérer les commandes et le support
 # Clic droit sur le rôle dans Discord > Copier l'identifiant
-STAFF_ROLE_ID = 123456789012345678
+STAFF_ROLE_ID = 1545858572196519957
 
 # ID de la catégorie où les salons de COMMANDES seront créés
-ORDER_CATEGORY_ID = 123456789012345678
+ORDER_CATEGORY_ID = 1545856735615647864
 
 # ID de la catégorie où les salons de SUPPORT seront créés
-SUPPORT_CATEGORY_ID = 123456789012345678
+SUPPORT_CATEGORY_ID = 1545856735615647864
 
 # ID du salon où seront envoyés les transcripts/logs quand un ticket est fermé
-LOG_CHANNEL_ID = 123456789012345678
+LOG_CHANNEL_ID = 1545857231717667065
